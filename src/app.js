28b34526cc34808c2c9a46f3ead2c284d23b8443
Michael Moore <michaelmoore@Michaels-MacBook-Pro.local>
2017-06-16 13:13:47 -0400

    rename file

--- src/app.js
+++ src/app.js
@@ -5,5 +5,5 @@ var angular = require('angular');
 
 angular.module('app', [])
     .factory('dataService', require('./data.service.js'))
-    .directive('cyGraph', require('./graph.directive.js'))
+    .directive('cyGraph', require('./cy-graph.directive.js'))
     .controller('AppCtrl', require('./app.controller.js'));
