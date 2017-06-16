b6649c707f37401c6ff59c7fc917e6c36fd458e9
Michael Moore <michaelmoore@Michaels-MacBook-Pro.local>
2017-06-15 21:48:54 -0400

    working on style

--- src/app.js
+++ src/app.js
@@ -4,6 +4,5 @@ require("style-loader!./stylesheet.css");
 var angular = require('angular');
 
 angular.module('app', [])
-    .constant('cytoscape', require('cytoscape'))
     .directive('mmGraph', require('./graph/graph.directive.js'))
     .controller('AppCtrl', require('./app.controller.js'));
--- src/graph/graph.directive.js
+++ src/graph/graph.directive.js
@@ -14,14 +14,20 @@ module.exports = function mmGraph() {
 
     function _build(elem) {
         var cy = cytoscape({
-          //container: document.getElementById('cy'),
             container: elem,
 
           boxSelectionEnabled: false,
           autounselectify: true,
 
           layout: {
-            name: 'dagre'
+            name: 'dagre',
+            nodeSep: 2,
+            edgeSep: 2,
+            rankSep: 2,
+            rankDir: 'LR',
+            animate: true,
+            animationDuration: 700,
+            animationEasing: 'ease-in-out'
           },
 
           style: [
@@ -30,18 +36,23 @@ module.exports = function mmGraph() {
               style: {
                 'content': 'data(id)',
                 'text-opacity': 0.5,
+                'width': 70,
+                'height': 30,
+                'shape': 'square',
                 'text-valign': 'center',
                 'text-halign': 'right',
-                'background-color': '#11479e'
+                'background-color': 'white',
+                'border-width': 0.25,
+                'border-color': '#b1b1b1'
               }
             },
 
             {
               selector: 'edge',
               style: {
-                'width': 4,
+                'width': 0.5,
                 'target-arrow-shape': 'triangle',
-                'line-color': '#9dbaea',
+                'line-color': '#b1b1b1',
                 'target-arrow-color': '#9dbaea'
               }
             }
@@ -49,38 +60,19 @@ module.exports = function mmGraph() {
 
           elements: {
             nodes: [
-              { data: { id: 'n0' } },
-              { data: { id: 'n1' } },
-              { data: { id: 'n2' } },
-              { data: { id: 'n3' } },
-              { data: { id: 'n4' } },
-              { data: { id: 'n5' } },
-              { data: { id: 'n6' } },
-              { data: { id: 'n7' } },
-              { data: { id: 'n8' } },
-              { data: { id: 'n9' } },
-              { data: { id: 'n10' } },
-              { data: { id: 'n11' } },
-              { data: { id: 'n12' } },
-              { data: { id: 'n13' } },
-              { data: { id: 'n14' } },
-              { data: { id: 'n15' } },
-              { data: { id: 'n16' } }
+                { data: { id: 'n0' } },
+                { data: { id: 'n1' } },
+                { data: { id: 'n2' } },
+                { data: { id: 'n3' } },
+                { data: { id: 'n4' } },
+                { data: { id: 'n5' } }
             ],
             edges: [
-              { data: { source: 'n0', target: 'n1' } },
-              { data: { source: 'n1', target: 'n2' } },
-              { data: { source: 'n1', target: 'n3' } },
-              { data: { source: 'n4', target: 'n5' } },
-              { data: { source: 'n4', target: 'n6' } },
-              { data: { source: 'n6', target: 'n7' } },
-              { data: { source: 'n6', target: 'n8' } },
-              { data: { source: 'n8', target: 'n9' } },
-              { data: { source: 'n8', target: 'n10' } },
-              { data: { source: 'n11', target: 'n12' } },
-              { data: { source: 'n12', target: 'n13' } },
-              { data: { source: 'n13', target: 'n14' } },
-              { data: { source: 'n13', target: 'n15' } },
+                { data: { source: 'n0', target: 'n1' } },
+                { data: { source: 'n1', target: 'n2' } },
+                { data: { source: 'n2', target: 'n3' } },
+                { data: { source: 'n4', target: 'n5' } },
+                { data: { source: 'n5', target: 'n2' } }
             ]
           },
         });
