7d88ae0659fa750e1856238308d52a2ff29566bb
Michael Moore <michaelmoore@Michaels-MacBook-Pro.local>
2017-06-16 13:12:54 -0400

    rm subdirs

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ require("style-loader!./stylesheet.css");
 var angular = require('angular');
 
 angular.module('app', [])
-    .factory('dataService', require('./data/data.service.js'))
-    .directive('cyGraph', require('./graph/graph.directive.js'))
+    .factory('dataService', require('./data.service.js'))
+    .directive('cyGraph', require('./graph.directive.js'))
     .controller('AppCtrl', require('./app.controller.js'));
