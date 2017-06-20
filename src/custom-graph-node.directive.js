3ccb2cd24c8464e333fe1cf56411e6af8d4583ce
Michael Moore <michaelmoore@Michaels-MacBook-Pro.local>
2017-06-20 16:26:59 -0400

    remove unused code

--- src/custom-graph-node.directive.js
+++ src/custom-graph-node.directive.js
@@ -6,16 +6,6 @@ function customGraphNode() {
         templateUrl: 'views/custom-graph-node.html',
         scope: {
             data: '='
-        },
-        link: function (scope, elem) {
-            console.log('customGraphNode ', scope.data);
-            console.log(scope.data.height, scope.data.width);
-            // elem.css({
-            //     width: scope.data.width + 'px',
-            //     height: scope.data.height  + 'px',
-            //     top: scope.data.y + 'px',
-            //     left: scope.data.x  + 'px'
-            // });
         }
     };
 }
